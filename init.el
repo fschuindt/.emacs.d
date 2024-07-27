53d512899c38145abda8b796f0b60672d1d729f0
fschuindt <f.schuindtcs@gmail.com>
2024-07-27 19:50:11 -0300

    feat: Use elixir-mode instead of elixir-ts-mode

--- init.el
+++ init.el
@@ -34,6 +34,7 @@
   (menu-bar-mode -1)
   (scroll-bar-mode -1)
   (tool-bar-mode -1)
+  (electric-pair-mode t)
 
   (defun kill-all-buffers ()
     "Reset Emacs to a clean buffer state."
@@ -41,6 +42,9 @@
     (mapc 'kill-buffer (buffer-list)))
 
   (global-set-key (kbd "C-c k") 'kill-all-buffers))
+
+(straight-use-package 'dockerfile-mode)
+(straight-use-package 'heex-ts-mode)
 
 (use-package flatui-theme
   :straight t
@@ -141,26 +145,29 @@
   :config
   (exec-path-from-shell-initialize))
 
-(use-package treesit
+(use-package elixir-mode
+  :straight t
+  :hook ((elixir-mode . eglot-ensure)
+        (before-save . eglot-format)))
+
+(use-package eglot
+  :ensure nil
+  :config
+  (add-to-list
+    'eglot-server-programs `((elixir-mode heex-mode) . ("start_lexical.sh")))
+  (add-hook
+    'elixir-mode-hook 'eglot-ensure)
+  (add-hook
+    'heex-mode-hook 'eglot-ensure)
+  (add-hook
+    'elixir-mode-hook (lambda () (add-hook 'before-save-hook 'eglot-format-buffer nil t))))
+
+(use-package sideline-flymake
+  :straight t
+  :hook (flymake-mode . sideline-mode)
   :init
-  (setq treesit-language-source-alist
-    '((bash "https://github.com/tree-sitter/tree-sitter-bash")
-       (cmake "https://github.com/uyha/tree-sitter-cmake")
-       (css "https://github.com/tree-sitter/tree-sitter-css")
-       (elisp "https://github.com/Wilfred/tree-sitter-elisp")
-       (elixir "https://github.com/elixir-lang/tree-sitter-elixir")
-       (go "https://github.com/tree-sitter/tree-sitter-go")
-       (heex "https://github.com/phoenixframework/tree-sitter-heex")
-       (html "https://github.com/tree-sitter/tree-sitter-html")
-       (javascript "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src")
-       (json "https://github.com/tree-sitter/tree-sitter-json")
-       (make "https://github.com/alemuller/tree-sitter-make")
-       (markdown "https://github.com/ikatyang/tree-sitter-markdown")
-       (python "https://github.com/tree-sitter/tree-sitter-python")
-       (toml "https://github.com/tree-sitter/tree-sitter-toml")
-       (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
-       (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-       (yaml "https://github.com/ikatyang/tree-sitter-yaml"))))
+  (setq sideline-flymake-display-mode 'point)
+  (setq sideline-backends-right '(sideline-flymake)))
 
 (use-package diff-hl
   :straight t
